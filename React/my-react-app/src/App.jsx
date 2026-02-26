39aa2fc57bc74b6f4b2ef45b1e9e3766febbedb1
tushar jana <145481506+Tushar3002@users.noreply.github.com>
2026-02-26 10:23:59 +0530

    React

--- React/my-react-app/src/App.jsx
+++ React/my-react-app/src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import Header from './Header'
+import Footer from './Footer'
 
 function App() {
-
+  const a="Tushar"
+  function nm(str){
+    return str
+  }
   return (
     <div className='App'>
     <Header/>
         <h1>Hello TJ</h1>
+        <h3>{a}</h3>
+        <h3>{nm("Jana")}</h3>
         
+        <Footer />
         </div>
   )
 }
