4a2de95aab12b8535c647b8116f48c3d06ba6ea0
tushar jana <145481506+Tushar3002@users.noreply.github.com>
2026-02-26 19:17:24 +0530

    Practice some React

--- React/my-react-app/src/main.jsx
+++ React/my-react-app/src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
+import Header from './Header.jsx'
+import Input from './Input.jsx'
+import Profile from './Profile.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   
   <StrictMode>
-    <App /> 
-    
+    {/* <App />  */}
+    {/* <Input /> */}
+    <Profile/>
   </StrictMode>,
 )
